22851c58e666a38cb1d48824517873c7a3cc6305
a.kucheev <ak-91@mail.ru>
2017-01-06 10:53:16 +0300

    Spring MVC WebSocket integration

--- src/main/java/ru/alcereo/pairlearning/SocketChat/ChatSocketConnection.java
+++ src/main/java/ru/alcereo/pairlearning/SocketChat/ChatSocketConnection.java
@@ -3,6 +3,8 @@ package ru.alcereo.pairlearning.SocketChat;
 import org.apache.tomcat.websocket.WsSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.server.standard.SpringConfigurator;
 import ru.alcereo.pairlearning.Service.Chat.ChatRoom;
 import ru.alcereo.pairlearning.SocketChat.exceptions.SocketConnectionConstructionException;
 
@@ -14,7 +16,7 @@ import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.Objects;
 
-@ServerEndpoint(value = "/socket")
+@ServerEndpoint(value = "/socket", configurator = SpringConfigurator.class)
 public class ChatSocketConnection {
 
     private static final Logger log = LoggerFactory.getLogger(ChatSocketConnection.class);
@@ -26,6 +28,7 @@ public class ChatSocketConnection {
     private ChatRoom chatRoom;
     private SocketSessionProvider socketSessionProvider;
 
+    @Autowired
     public void setSocketSessionProvider(SocketSessionProvider socketSessionProvider) {
         this.socketSessionProvider = socketSessionProvider;
     }
